0a1aabc9cb93e5e1c020da34db41c87cadf6468b
KaliuzhnaMariia <124194241+KaliuzhnaMariia@users.noreply.github.com>
2023-10-01 15:21:15 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Java Lab 2 ");
+        System.out.println("Test ");
 	// write your code here
 
     }
